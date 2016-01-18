6235dd15e48c5f38da889458265125814dc89867
Hamilton Greene <hamilton.greene@gmail.com>
2016-01-17 22:38:47 -0500

    Added initial spiral code

--- ObeyTheGum.js
+++ ObeyTheGum.js
@@ -2,34 +2,42 @@
 
 //**P5-built Function Code**
 
+//Images
 var obeyFace
+var logo;
+
+//Sketch vars
 var fr = 30;
 var timeAllot = 4;
 
+//Sketch state
 var duration;
 var counter;
 
+//Bubble vars
 var gumType;
 var bubblePt1;
 var bubblePt2;
 
 function preload() {
   obeyFace = loadImage("images/ObeyFace2.png");
+  logo = loadImage("images/LogoFat.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //createCanvas(windowWidth, windowHeight, WEBGL);
   
-  //Resize obeyFace so it sits on screen
-  //obeyFace.resize((windowHeight/obeyFace.height) * obeyFace.width, windowHeight);
-  
+  //Sketch
   gumType = 0;
   duration = timeAllot * fr;
   counter = 1;
   stroke("#000000");
   
+  //P5
   frameRate(fr);
   
+  //Bubble
   bubblePt1 = new Pt(windowWidth/2, windowHeight/2);
 }
 
@@ -41,9 +49,6 @@ class Pt{
 };
 
 function draw() {
-  if(counter == duration) {
-    counter = 1;
-  }
   
   background("#000000");
   image(obeyFace, (windowWidth/2) - (((windowHeight/obeyFace.height) * obeyFace.width)/2), 0, (windowHeight/obeyFace.height) * obeyFace.width, windowHeight);
@@ -52,15 +57,13 @@ function draw() {
   fill('#ff8491');
   
   if(gumType == 0) {
-    if( counter <= duration) {
-      drawBubble(counter, duration);
-    }
-    
-    /*
+    drawBubble(counter, duration);
+
     if(counter == duration) {
       gumType = 1;
+      counter = 1;
     }
-    */
+    
     /*
     else if(counter > duration) {
       //popBubble();
@@ -69,7 +72,12 @@ function draw() {
     }
     */
   } else if(gumType == 1) {
+    drawSpiral(counter, duration);
     
+    if(counter == 3 * duration) {
+      gumType = 0;
+      counter = 1;
+    }
   }
   
   counter++;
@@ -82,6 +90,24 @@ function windowResized() {
 
 //**Bubble Code**
 
+var logoBubble = function(counter, duration, bubbleSize) {
+  var logoWidth = ( (3/4) * 1.5 * bubbleSize)
+  var logoHeight = (logoWidth/logo.width) * logo.height;
+  image(logo, windowWidth/2 - logoWidth/2, windowHeight/2 - logoHeight/2, logoWidth, logoHeight);
+  /*var logoShape = beginShape();
+  texture(logo);
+  vertex(windowWidth/2 - logoWidth/2, windowHeight/2 - logoHeight/2);
+  vertex(windowWidth/2 + logoWidth/2, windowHeight/2 - logoHeight/2);
+  vertex(windowWidth/2 + logoWidth/2, windowHeight/2 + logoHeight/2);
+  vertex(windowWidth/2 - logoWidth/2, windowHeight/2 + logoHeight/2);
+  endShape(CLOSE);
+  */
+  
+}
+
+/** drawBubble
+ * Main code to create the bubble
+ **/
 var drawBubble = function(counter, duration) {
   var maxSize = windowHeight/2;
   var bubbleSize = maxSize * Math.pow((counter/duration), .6);
@@ -89,9 +115,11 @@ var drawBubble = function(counter, duration) {
   //drawShine(bubbleSize);
   
   ellipse(windowWidth/2, windowHeight/2, 1.5*bubbleSize, bubbleSize);
+  
+  logoBubble(counter, duration, bubbleSize);
 }
 
-/*
+/* drawTeardrop
 xVal - x coordinate to draw tearDrop point
 yVal - y coordinate to draw tearDrop point
 height - Desired length of teardrop from point to end of bulb
@@ -159,8 +187,24 @@ var popBubble = function() {
 //**Spiral Code**
 
 var drawSpiral = function(counter, duration) {
+  var maxCirRadius = logo.height;
   
+  var radius = 0;
+  var radiusInc = maxCirRadius / duration;
+  
+  var theta = 0.0;
+  var thetaInc = (3.84 * PI ) / duration;
+  
+  for(var i = 0; i<counter; i++) {
+    var x = cos(theta) * radius + windowWidth/2;
+    var y = sin(theta) * radius + windowHeight/2;
+    ellipse(x, y, 50, 50);
+    theta += thetaInc;
+    radius += radiusInc;
+  }
 }
+
+//**Despiral Code**
 
 //**Helper Code**
 
